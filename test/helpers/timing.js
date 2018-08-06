dd5609b8c8504f4afa59c2c7caebd091f0f31caa
Vinson Chuong <vinsonchuong@gmail.com>
2018-08-06 10:13:23 -0700

    chore(travis): Remove test so that Travis will not hang

--- test/helpers/timing.js
+++ test/helpers/timing.js
@@ -2,16 +2,3 @@
 import { promisify } from 'util'
 
 export const sleep = promisify(setTimeout)
-
-export async function waitForCondition(
-  predicate: () => boolean,
-  timeout: number = 5000
-) {
-  if (predicate()) {
-  } else if (timeout <= 0) {
-    throw new Error('Condition never satisfied')
-  } else {
-    await sleep(500)
-    await waitForCondition(predicate, timeout - 500)
-  }
-}

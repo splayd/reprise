cde3ff4688f25e0ca9c69fdd97084f7d1fcb9cca
Vinson Chuong <vinsonchuong@gmail.com>
2018-08-06 08:32:15 -0700

    chore(travis): Prevent tests from hanging on Travis

--- test/helpers/timing.js
+++ test/helpers/timing.js
@@ -3,8 +3,14 @@ import { promisify } from 'util'
 
 export const sleep = promisify(setTimeout)
 
-export async function waitForCondition(predicate: () => boolean) {
-  if (!predicate()) {
+export async function waitForCondition(
+  predicate: () => boolean,
+  timeout: number = 5000
+) {
+  if (predicate()) {
+  } else if (timeout <= 0) {
+    throw new Error('Condition never satisfied')
+  } else {
     await sleep(100)
     await waitForCondition(predicate)
   }
